bda5a4255bb9036cb8a991b4fc6079811fb2cc0d
Max-Bero <maxberovski@gmail.com>
2026-04-18 17:54:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# React + TypeScript + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Oxc](https://oxc.rs)
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/)
+
+## React Compiler
+
+The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
+
+# Todo App By Maxim Project for DSS 📋
+
+A modern, management application featuring a glass-like aesthetic, dark mode support, and suttle animations.
+
+## 📸 Preview
+![App Screenshot](todoapp_screenshot.png) 
+![App Screenshot](todoapp_screenshot_dark.png) 
+
+## 🚀 Features
+- **Fluid UI:** Modern glass-like backdrop filters and squircle styling.
+- **Dark Mode:** Seamless toggle between light and dark themes.
+- **Task Management:** Add, edit, and delete tasks with instant UI feedback.
+- **Sorting function:** Sort by alphabetical order and by date completed.
+- **Filtering:** Filter tasks by user.
+- **Responsive Design:** Optimized for mobile and desktop screens.
+
+## 🛠 Prerequisites
+To run this project, you need to have the following installed on your machine:
+- **Node.js** (v14.0.0 or higher recommended)
+- **npm** (comes with Node.js) or **yarn**
+
+## 📦 Installation
+1. Clone the repository:
+   ```bash
+   git clone [https://github.com/YOUR_USERNAME/YOUR_REPO_NAME.git](https://github.com/Max-Bero/TODO-APP-DSS-Project.git)
---
+++ src/App.tsx
@@ -0,0 +1,324 @@
+import './App.css'
+import { useState, useEffect } from 'react';
+
+
+function App (){
+  interface Task{
+    userid: number;
+    id: string;
+    text: string;
+    date: number;
+    isDone: boolean;
+  }
+
+  const [todos, setTodos] = useState<Task[]>([]);
+  const [ogtodos, setogTodos] = useState<Task[]>([]);
+  const [taskText, setTaskText] = useState<string>("");
+  const [completed, setCompleted] = useState<Task[]>([]); 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [visibleCount, setVisibleCount] = useState<number>(5);
+  const [visibleCompletedCount, setVisibleCompletedCount] = useState<number>(5);
+  const [taskToDelete, setTaskToDelete] = useState<number | null>(null);
+  const fetchdatasize = 50;
+  
+  useEffect(() => {
+    const getTasks = async () => {
+      setIsLoading(true); 
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+
+        const mappedData: Task[] = data.slice(0, fetchdatasize).map((item: any) => ({
+          userid: item.userId,
+          id: String(item.id),
+          text: item.title,
+          date: Date.now(),
+          isDone: item.completed 
+        }));
+
+        const uncompletedTasks = mappedData.filter(task => !task.isDone);
+        const completedTasks = mappedData.filter(task => task.isDone);
+
+        setTodos(uncompletedTasks);
+        setCompleted(completedTasks);
+        setogTodos(uncompletedTasks);
+
+
+      } catch (error) {
+        console.error("Failed to fetch:", error);
+      } finally {
+        setIsLoading(false); 
+      }
+    };
+
+    getTasks();
+  }, []);
+
+  const handleAddTask = (event: React.SubmitEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    if (taskText.trim() === "") return;
+
+    console.log("Adding task:", taskText);
+
+    const maxId = ogtodos.length > 0 
+    ? Math.max(...ogtodos.map(t => Number(t.id))) 
+    : 0;
+
+  const nextId = String(maxId + 1);
+  
+    const newTask: Task = {
+      userid: 0, //0 for local user adding from the form
+      id: nextId,
+      text: taskText,
+      date: Date.now(),
+      isDone: false,
+    };
+
+    setTodos([newTask, ...todos]);
+    setogTodos(prevOg => [newTask, ...prevOg]);
+    setTaskText("");
+  }
+
+  const deleteTask = (index:number)=>{
+    const taskToDelete = completed[index];
+    const newCompleted = completed.filter((_, i)=>i!==index);
+    setCompleted(newCompleted);
+    setogTodos(ogtodos.filter(t => t.id !== taskToDelete.id));
+  }
+
+  const completeTask = (id:string)=>{
+    const taskToMove = todos.find(t => t.id === id);
+    if (taskToMove) {
+      const updatedTask = { 
+      ...taskToMove, 
+      date: Date.now(),
+      isDone: true
+      };  
+
+      setTodos(todos.filter(t => t.id !== id));
+      setCompleted([updatedTask, ...completed ]);
+      setogTodos(ogtodos.map(t => t.id === id ? updatedTask : t));
+    }
+    else return;
+  }
+
+  const undoComplete = (index:number)=>{
+    const taskToUndo = completed[index];
+    if (taskToUndo) {
+      const originalTask = ogtodos.find(t => t.id === taskToUndo.id);
+      const updatedTask = {
+        ...taskToUndo,
+        date: originalTask ? originalTask.date : taskToUndo.date
+      }
+      const nextTodos = [updatedTask, ...todos];
+      nextTodos.sort((a, b) => Number(a.id) - Number(b.id));
+
+      const newCompleted = completed.filter((_, i) => i !== index);
+      setCompleted(newCompleted);
+      setTodos(nextTodos); 
+    } else return;
+  }
+
+  const sortTodos = (type:string) => {
+    let sorted = [...todos];
+
+    if (type === 'asc') {
+      sorted.sort((a, b) => a.text.localeCompare(b.text));
+    } 
+    else if (type === 'desc') {
+      sorted.sort((a, b) => b.text.localeCompare(a.text));
+    } 
+    else if (type === 'default') {
+      sorted.sort((a, b) => Number(a.id) - Number(b.id));
+    }
+
+    setTodos(sorted);
+  }
+
+const filterByUser = (user: number) => {
+ let filtered: Task[];
+
+  if (user === 0) {
+    filtered = ogtodos.filter(task => !task.isDone);
+  } else if (user === -1) {
+    filtered = ogtodos.filter((task) => task.userid === 0 && !task.isDone);
+  } else {
+    filtered = ogtodos.filter((task) => task.userid === user && !task.isDone);
+  }
+
+  setTodos(filtered);
+  }
+
+const countUsers = () => {
+  const userIds = ogtodos.map(task => task.userid);
+  const uniqueUserIds = new Set(userIds);
+  const numberOfUsers = uniqueUserIds.size;
+  return numberOfUsers;
+}
+
+const sortByDate = (type: string) => {
+  let sorted = [...completed];
+  if (type === 'newest') {
+    sorted.sort((a, b) => b.date - a.date);
+  } 
+  else if (type === 'oldest') {
+    sorted.sort((a, b) => a.date - b.date);
+  }
+
+  setCompleted(sorted);
+}
+
+const [theme, setTheme] = useState("light");
+
+useEffect(() => {
+  document.body.setAttribute('data-theme', theme);
+}, [theme]);
+
+const toggleTheme = () => {
+  setTheme(prev => (prev === "light" ? "dark" : "light"));
+};
+
+  return (
+    <>
+    <div className="main-container">
+      <header>
+
+        <label  className="switch">
+          <input  type="checkbox" onChange={toggleTheme}/>
+          <span className="slider round"> </span>
+        </label>
+
+        <h1> Todo App</h1>
+
+      </header>
+
+      <main>
+        <form className='task-form' onSubmit={handleAddTask}>
+          <label htmlFor="task-input">Type task: </label>
+          <input 
+            id="task-input" 
+            type="text" 
+            value = {taskText} 
+            onChange={(e)=>setTaskText(e.target.value)} 
+          />
+          <button type="submit">Add</button>
+        </form>
+  
+        <div className='left-right'>
+          {isLoading ? (<h1 className='loading-message'>Loading tasks please wait...</h1>) : (
+          <>
+          <div id='left-side'>
+            <h2>Uncompleted tasks: </h2>
+
+            Sort by: 
+            <select name='Sort' onChange={(e) => sortTodos(e.target.value)}>
+                <option value='default'>Default</option>
+                <option value='asc'>Alphabetical (asc.)</option>
+                <option value='desc'>Alphabetical (desc.)</option>    
+            </select>
+
+            Filter:
+            <select name='Filter' onChange={(e) => filterByUser(Number(e.target.value))}>
+              <option value={0}>All Users</option>
+              <option value={-1}>local</option>
+  
+              {Array.from({ length: countUsers() }, (_, i) => (
+                <option key={i + 1} value={i + 1}>
+                  User {i + 1}
+                </option>
+                ))}
+            </select>
+
+
+            <ul id='uncompleted-list'>
+              {todos.slice(0, visibleCount).map((task) => (
+              <li key={task.id}>
+                <div>
+                  <strong>{task.text}</strong>
+                  <p style={{ fontSize: '12px'}}>
+                    Added: {new Date(task.date).toLocaleString()}
+                  </p>
+                </div>
+                <button onClick={() => completeTask(task.id)}>Done</button>
+              </li>
+              ))}
+            </ul> 
+
+            <div className='show-more'>
+            {visibleCount < todos.length && (
+              <button onClick={() => setVisibleCount(prev => prev + 5)}>
+                Show More
+              </button>
+            )}
+            </div>
+
+          </div>
+
+          <div id="right-side">
+            <h2>Completed tasks: </h2>
+            Sort by: 
+            <select name='Sort' onChange={(e) => sortByDate(e.target.value)}>
+                <option value='newest'>Newest</option>
+                <option value='oldest'>Oldest</option>  
+            </select>
+
+            <ul id='completed-list'>
+              {completed.slice(0, visibleCompletedCount).map((task, index)=>(
+                <li key={task.id} >
+                  <div>
+                    <strong style={{textDecoration: 'line-through'}}>{task.text}</strong>
+                    <p style={{fontSize: '12px'}}>
+                      Completed: {new Date(task.date).toLocaleString()}
+                    </p>
+                  </div>
+
+                  <div className='undo-del-buttons'>
+                    <button className='undo' onClick={()=>undoComplete(index)}>Undo</button>
+                    <button className='del' onClick={()=>setTaskToDelete(index)}>Delete</button>
+                  </div>
+                  
+                </li>
+              ))}
+            </ul>
+
+            <div className='show-more'>
+            {visibleCompletedCount < completed.length && (
+              <button onClick={() => setVisibleCompletedCount(prev => prev + 5)}>
+                Show More
+              </button>
+            )}
+            </div>
+            
+          </div>
+          </>
+          )}
+        </div>
+      </main>
+    </div>
+
+
+     {taskToDelete !== null && (
+      <div className="modal-overlay">
+        <div className="confirmation-card">
+          <div className="icon-warning">⚠️</div>
+          <h3>Are you sure?</h3>
+          <p>This action cannot be undone. Do you really want to delete this task?</p>
+          
+          <div className="modal-buttons">
+            <button className="confirm-btn" onClick={() => {
+              deleteTask(taskToDelete); // Your existing delete function
+              setTaskToDelete(null);    // Close the window
+            }}>
+              Delete
+            </button>
+            <button className="cancel-btn" onClick={() => setTaskToDelete(null)}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    )} 
+  </> 
+  )
+}
+export default App;
